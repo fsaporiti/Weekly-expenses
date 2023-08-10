b714f1f8406b09bd2c4bc2d2d5a756d0805c1f38
facundo <facundo.saporiti@hotmail.com>
2023-08-10 16:12:10 -0300

    working on Js for front-end and local storage

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
 const form = document.querySelector("#transactionForm")
 
-const insertRowTable = (transactionFormData) => {
+const insertRowTable = (transactionObj) => {
     let tableRef = document.querySelector("#table")
-    //insert row 
-    let newRowRef = tableRef.insertRow(-1)
-
-    //nsert cell (in the positions 0, 1, 2, 3)
-    let newCellRef = newRowRef.insertCell(0)
-
-    //add the text put in the form in each cell
-    newCellRef.textContent = transactionFormData.get("formIncomeExpense")
+    let newRowRef = tableRef.insertRow(-1) //insert row 
+    let newCellRef = newRowRef.insertCell(0)  //insert cell (in the positions 0, 1, 2, 3)
+    newCellRef.textContent = transactionObj["formIncomeExpense"] //add the text put in the form in each cell
     newCellRef = newRowRef.insertCell(1)
-    newCellRef.textContent = transactionFormData.get("formDescription")
+    newCellRef.textContent = transactionObj["formDescription"]
     newCellRef = newRowRef.insertCell(2)
-    newCellRef.textContent = transactionFormData.get("formAmount")
+    newCellRef.textContent = transactionObj["formAmount"]
     newCellRef = newRowRef.insertCell(3)
-    newCellRef.textContent = transactionFormData.get("formCategory")
+    newCellRef.textContent = transactionObj["formCategory"]
 }
 
+const convertFormDataToObj = (transactionFormData) => {
+    let formIncomeExpense = transactionFormData.get("formIncomeExpense")
+    let formDescription = transactionFormData.get("formDescription")
+    let formAmount = transactionFormData.get("formAmount")
+    let formCategory = transactionFormData.get("formCategory")
+    //return obj with the content in the form
+    return { 
+        "formIncomeExpense": formIncomeExpense,
+        "formDescription": formDescription,
+        "formAmount": formAmount,
+        "formCategory": formCategory
+    }
+}
+
+const saveTransactionObj = (transactionObj) => {
+    let transactionArray = JSON.parse(localStorage.getItem("transactionData")) || []
+    transactionArray.push(transactionObj)
+    let arrayToJSON = JSON.stringify(transactionArray) // convert array to JSON
+    localStorage.setItem("transactionData", arrayToJSON) //save my array-JSON to LStorage 
+}
+
+
 form.addEventListener ("submit", (e) => {
-   e.preventDefault()
+    e.preventDefault()
+    let transactionFormData = new FormData(form)
+    let transactionObj = convertFormDataToObj(transactionFormData)
+    saveTransactionObj (transactionObj) // save data in LStorage
+    insertRowTable(transactionObj) // insert ROW with form's data
+ 
+    })
 
-   let transactionFormData = new FormData(form)
-   insertRowTable(transactionFormData)
-
-   })
 
    
